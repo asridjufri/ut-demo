7389bcfc589362f7e4456a1c2045661b953a48ea
asrijuhas <asri@trinix.id>
2018-08-16 10:17:21 +0700

    --as

--- hello.go
+++ hello.go
@@ -12,7 +12,7 @@ func handler(w http.ResponseWriter, r *http.Request) {
 }
 func trinixHandler(w http.ResponseWriter, r * http.Request){
 	fmt.Println("/trinix")
-	fmt.Fprintf(w, "Hi Iam Trinix Systemsss %s!", r.URL.Path[1:])	
+	fmt.Fprintf(w, "Hi Iam Trinix Systemsssas %s!", r.URL.Path[1:])	
 }
 func utHandler(w http.ResponseWriter, r * http.Request){
 	fmt.Println("/ut")
